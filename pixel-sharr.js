c49752e1d7b1800288887140ca2b2663bc88950e
Zach Bergson <zach.bergson@gmail.com>
2016-01-27 18:43:07 -0500

    mvp almost done

--- pixel-sharr.js
+++ pixel-sharr.js
@@ -17,16 +17,53 @@ if (Meteor.isClient) {
       // Get value from form element
       var content = $('.content').val();
       var image = $('.image').val();
+      var likes = 0;
       
       
  
       // Insert an image into the collection
-      Meteor.call("addPic", image, content);
+      Meteor.call("addPic", image, content, likes);
  
       // Clear form
       $('.content').val('');
       $('.image').val('');
+    },
+    "click .userpage": function() {
+      var pictures = $('.pictures');
+      
+      
+      for (i = 0; i < pictures.length; i++ ) {
+        var picturesLoop = pictures[i]
+        
+        var changePictures = $('.pictures')[i];
+        if ($(picturesLoop).attr('data-id') !== Meteor.userId()) {
+          $(changePictures).hide();
+          $('header').css("background-image", "url(http://videos.revision3.com/revision3/images/shows/lilbub/0052/lilbub--0052--the-return-of-bubs-real-brother-the-spooky-saga--marge.thumb.jpg)")
+        }
+
+      }
+    },
+    "click .home": function() {
+      console.log('hi');
+      $('.pictures').show();
+      $('header').css("background-image", "none");
+    },
+    "click .username": function() {
+      var pictures = $('.pictures');
+      var currentOwner = this.owner;
+      
+
+      for (i = 0; i < pictures.length; i++) {
+        var picturesLoop = pictures[i];
+        var changePictures = $('.pictures')[i];
+
+        if($(picturesLoop).attr('data-id') !== currentOwner) {
+          $(changePictures).hide();
+        }
+
+      }
     }
+    
 
   });
 
@@ -36,8 +73,15 @@ if (Meteor.isClient) {
 
   Template.pic.events({
     "click .delete": function () {
-      Meteor.call("deletePic", this._id);
+      if (this.owner === Meteor.userId()) {
+        Meteor.call("deletePic", this._id);
+      }
+      
+    },
+    "click .like-button": function() {
+      Meteor.call("clickLike", this._id);
     }
+
   });
 
 
@@ -59,7 +103,7 @@ if (Meteor.isClient) {
 
 
 Meteor.methods({
-  addPic: function (image, content) {
+  addPic: function (image, content, likes) {
     // Make sure the user is logged in before inserting image
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
@@ -70,11 +114,17 @@ Meteor.methods({
       content: content,
       createdAt: new Date(),
       owner: Meteor.userId(),
-      username: Meteor.user().username
+      username: Meteor.user().username,
+      likes: likes
     });
   },
   deletePic: function (picId) {
     Pics.remove(picId);
+  },
+  clickLike: function(picId) {
+    Pics.update(picId, {
+      $inc: {likes: +1}
+    });
   }
 });
 
